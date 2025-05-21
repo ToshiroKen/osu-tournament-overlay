3fd4432b4c025ddc6d4ed8d8604cb43e630ec210
DaijobuDes <git@daijobudes.net>
2025-05-21 17:39:54 +0800

    Configure vite to use /dist/ path by default for faster use

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,9 +1,17 @@
 import { defineConfig } from 'vite';
+import { resolve } from "path";
 
 export default defineConfig({
+    // Change this if you have different path for tosu
+    base: 'dist',
     plugins: [],
     build: {
         sourcemap: true,
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, "index.html"),
+            },
+        },
     },
     css: {
         devSourcemap: true,
